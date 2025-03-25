59bed7e8763c42e02f5d3125c8470b6be416d717
Jose Francisco <josmurfue@alu.edu.gva.es>
2025-03-25 20:16:42 +0100

    Completar ejercicios

--- ejercicio2b.py
+++ ejercicio2b.py
@@ -2,15 +2,22 @@
 #Pide la edad y el nombre por teclado. En caso de ser mayor de 18 años indica que ya puede conducir.
 
 # Implemente función esMayorEdad(e)
-
-
+def esMayorEdad(edad,nombre) :
+    if edad >= 18 :
+        print('Ya puedes conducir '+nombre)
+        return True
+    else :
+        print('No puedes conducir eres menor de edad ' +nombre)
+        return False
 # Programa principal
 def main():
-    nombre="Mariano";
-    edad=0;
+    nombre=input("Introduzca su nombre: ");
+    edad=int(input(f"Introduzca su edad {nombre}: "));
 
     # Utilice la función definida
+    esMayorEdad(edad,nombre)
     # Estructura alternativa Si (condidición con función) entonces --> sino ...
+
 
 if __name__== "__main__" :
    main()
--- ejercicio3.py
+++ ejercicio3.py
@@ -12,14 +12,31 @@ __Author__="José Gaspar Sánchez García"
 - Impltemente una función obtenerCalificacion(nota)."""
 
 # Implemente función obtenerCalificacion
-def obtenerCalificaion(nota) :
-    calificacion="Incorrecta"
+def obtenerCalificaion(n) :
    # Implemente aquí ... Si (condición) entonces ... sino ... si (condición) entonces ... sino ...
+    if   n >= 0 and n <=2 :
+        calificacion= "Muy deficiente"
+    elif n >= 3 and n <= 4 :
+        calificacion= "Insuficiente"
+    elif n >= 5 and n < 6 :
+        calificacion= "Suficiente"
+    elif n >= 6 and n < 7 :
+        calificacion= "Bien"
+    elif n >= 7 and n < 9 :
+        calificacion= "Notable"
+    elif n >= 9 and n <= 10 :
+        calificacion= "Sobresaliente"
+    elif n >= 11 :
+        calificacion="Incorrecta"
+    elif n < 0 :
+        calificacion="Incorrecta"
+        
     return calificacion
 
 # Programa principal
 def main():
     n=int(input("Introduzca la nota: "));
     print('Calificación: '+obtenerCalificaion(n));
+
 if __name__== "__main__" :
    main()
